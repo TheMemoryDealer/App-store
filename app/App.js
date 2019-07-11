22a23797faf8543755ad6219f432254138188730
Maz <mazvydas66@gmail.com>
2019-07-11 15:58:47 +0100

    Albums are displayed, redirected to sellers website once pressed

--- app/App.js
+++ app/App.js
@@ -1,19 +1,16 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Header from './src/components/header';
+import Header from './src/components/Header';
+import Albumlist from './src/components/AlbumList';
 
 
-export default function App() {
-  return (
-    <Header headerText={'Albums'}/>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <Header headerText={'Albums'} />
+        <Albumlist />
+      </View>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- app/src/components/header.js
+++ app/src/components/header.js
@@ -14,7 +14,7 @@ const styles = {
       backgroundColor: '#F8F8F8',
       justifyContent: 'center',
       alignItems: 'center',
-      height: 60,
+      height: 70,
       paddingTop: 15,
       shadowColor: '#000',
       shadowOffset: { width: 0, height: 2 },
